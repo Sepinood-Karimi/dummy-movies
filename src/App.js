1d8962ba1ba89a60376c12150877ffd7aeb72b0f
Sepinoud Karimi <sepinoodkarimi@gmail.com>
2022-08-31 19:48:40 +0430

    post request

--- src/App.js
+++ src/App.js
@@ -14,20 +14,24 @@ function App() {
             setIsLoading(true);
             setError(null);
             try {
-                const response = await fetch('https://swapi.dev/api/films');
+                const response = await fetch('https://dummymovies-b1b4b-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json');
                 if (!response.ok) {
                     throw new Error('Something Went Wrong!');
                 }
                 const data = await response.json();
-                const transformedMovies = data.results.map((movieData) => {
-                    return {
-                        id: movieData.episode_id,
-                        title: movieData.title,
-                        openingText: movieData.opening_crawl,
-                        releaseDate: movieData.release_date
-                    }
-                })
-                setMovies(transformedMovies);
+
+                const loadedMovies = [];
+                for (const key in data){
+                    loadedMovies.push({
+                        id : key,
+                        title : data[key].title,
+                        openingText: data[key].openingText,
+                        releaseDate: data[key].releaseDate
+                        }
+
+                    )
+                }
+                setMovies(loadedMovies);
             } catch (error) {
                 setError(error.message)
             }
@@ -51,8 +55,14 @@ function App() {
         content = <p> {error}</p>
     }
 
-    const addMovieHandler = () => {
-
+    const addMovieHandler = async (movie) => {
+        const response = await fetch('https://dummymovies-b1b4b-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json',{
+            method:'Post',
+            body : JSON.stringify(movie),
+            headers : {
+                'Content-Type': 'movies/json'
+            }
+        })
     }
 
     return (
